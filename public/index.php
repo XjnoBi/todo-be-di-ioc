7bd75a1507d941e33dd668ce82b9cbb8f1842187
XjnoBi <playmaker0720@gmail.com>
2024-03-08 21:22:17 +0800

    fix: handle cors origin

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php
 
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token");
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use App\Controllers\TodoController;
